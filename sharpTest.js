6363ed303e3bc8335dc1158ac7ace88893c0925d
Gary Davies <gary.davies01@bbc.co.uk>
2018-05-14 16:57:08 +0100

    parsed uint8 to int

--- sharpTest.js
+++ sharpTest.js
@@ -1,47 +1,31 @@
 const sharp = require('sharp');
 const request = require('request')
-var src = 'https://ichef.bbci.co.uk/redbutton-ichef/1920x1080/p0663jmv.jpg';
+var src = 'https://rawgit.com/drillep/imageLogo/master/5050.png'
 
 
 function modifyImage(src, dest, width, height) {
   request(src, { encoding: null }, function process(error, response, body) {
     if (error) return console.error(error);
       sharp(body)
-        .resize(width, height)
         .greyscale()
-        .extract({ left: 0, top: 0, width: 308, height: 156 })
-        //.crop(sharp.strategy.entropy)
+        .resize(1, 1)
+        //.extract({ left: 0, top: 0, width: 308, height: 156 })
         //.pipe(roundedCornerResizer)
         .raw()
         .toBuffer(function(err, data) {
-          // data is a Buffer containing uint8 values (0-255)
-          // with each byte representing one pixel
-          console.log(data)
-          var myImg = data.map(parseInt)
-          console.log(myImg)
+          var total = 0
+          var subtotal
+          for (b of data) {
+            console.log(parseInt(b,10))
+            total += parseInt(b,10)
+          }
+          console.log('the total is ' + total)
         })
         // .toFile(dest);
     });
 }
 
-modifyImage(src, 'modifiedImage.png', 1920, 1080);
-
-// sharp(Buffer)
-//   .resize(320, 240)
-//   .toFile('output.webp', (err, info) => {} );
-//   // A Promises/A+ promise is returned when callback is not provided.
-// sharp('dssm.png')
-//   .rotate()
-//   .resize(200)
-//   .toBuffer()
-//   .then( data => {} )
-//   .catch( err => {} );
-
-const cropTopRight =
-  sharp()
-    .resize(200, 200)
-    .crop(sharp.gravity.north)
-    .png();
+modifyImage(src, 'modifiedImage.png', 189, 189);
 
 const roundedCorners = new Buffer(
   '<svg><rect x="0" y="0" width="200" height="200" rx="50" ry="50"/></svg>'
@@ -52,7 +36,3 @@ const roundedCornerResizer =
     // .resize(200, 200)
     .overlayWith(roundedCorners, { cutout: true })
     .png();
-
-// readableStream
-//   .pipe(roundedCornerResizer)
-//   .pipe(writableStream);
